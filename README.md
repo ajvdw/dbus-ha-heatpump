af84d1d5ce46bca2431b7605a40a5a28dccf45f0
ajvdw <ajvandewerken@gmail.com>
2025-08-12 19:46:59 +0200

    Initial

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-# dbus-ha-evcharger
-Integrate your EVCharger from HomeAssistant into [Victron Energy Venus OS](https://github.com/victronenergy/venus)
+# dbus-ha-heatpump
+Integrate your heatpump from HomeAssistant into [Victron Energy Venus OS](https://github.com/victronenergy/venus)
 
 ## Purpose
-With the scripts in this repo it should be possible to add the EVCharger to VenusOS. 
+With the scripts in this repo it should be possible to add the heatpump to VenusOS. 
 
 ## Origin
 This repo is based on similar projects for shelly and home wizard integration
 
 ## Install & Configuration
 ### Get the code
-Just grap a copy of the main branche and copy them to `/data/dbus-ha-evcharger`.
+Just grap a copy of the main branche and copy them to `/data/dbus-ha-heatpump`.
 After that call the install.sh script.
 
 The following script should do everything for you:
 ```
-wget https://github.com/ajvdw/dbus-ha-evcharger/archive/refs/heads/main.zip
-unzip main.zip "dbus-ha-evcharger-main/*" -d /data
-mv /data/dbus-ha-evcharger-main /data/dbus-ha-evcharger
-chmod a+x /data/dbus-ha-evcharger/install.sh
-/data/dbus-ha-evcharger/install.sh
+wget https://github.com/ajvdw/dbus-ha-heatpump/archive/refs/heads/main.zip
+unzip main.zip "dbus-ha-heatpump-main/*" -d /data
+mv /data/dbus-ha-heatpump-main /data/dbus-ha-heatpump
+chmod a+x /data/dbus-ha-heatpump/install.sh
+/data/dbus-ha-heatpump/install.sh
 rm main.zip
 ```
 ⚠️ Check configuration after that - because service is already installed an running and with wrong connection data (host, username, pwd) you will spam the log-file
@@ -29,25 +29,23 @@ Copy the following to configuration.yaml in HomeAssistant
 ```
 template:
   - sensor:
-      - name: "evcharger_json"
-        unique_id: "evcharger_01_json"
+       - name: "heatpump_json"
+        unique_id: "heatpump_json"
         unit_of_measurement: "kWh"
         state_class: measurement
-        state: "{{ states('sensor.evcharger_daily')|float }}"
+        state: "{{ states('sensor.heatpumpdaily')|float }}"
         attributes:
-          power: "{{ states('sensor.evcharger_power') }}"
-          energy: "{{ states('sensor.evcharger_total_energy')}}"
-          l1_v: "{{ states('sensor.evcharger_phase_a_voltage') }}"
-          l2_v: "{{ states('sensor.evcharger_phase_b_voltage') }}"
-          l3_v: "{{ states('sensor.evcharger_phase_c_voltage') }}"
-          l1_i: "{{ states('sensor.evcharger_phase_a_current') }}"
-          l2_i: "{{ states('sensor.evcharger_phase_b_current') }}"
-          l3_i: "{{ states('sensor.evcharger_phase_c_current') }}" 
-          unique_id: "000002"
+          state: "{{ states('switch.manualboiler') }}"
+          power: "{{ (((states('sensor.eastron_sdm_total_power')) | float) * 1000)|round(1) }}"
+          energy: "{{ ((states('sensor.eastron_sdm_import_active_energy')) | float) |round(1) }"
+          current_temp: "{{ states('sensor.ebusd_ctlv2_hwcstoragetemp_tempv')|round(1)}}"
+          target_temp: "{{ states('number.ebusd_ctlv2_hwctempdesired_tempv') }}"
+          unique_id: "000003"          
+
 ```
 
 ### Change config.ini
-Within the project there is a file `/data/dbus-ha-evcharger/config.ini` - just change the values - most important is the host
+Within the project there is a file `/data/dbus-ha-heatpump/config.ini` - just change the values - most important is the host
 
 | Section  | Config vlaue | Explanation |
 | ------------- | ------------- | ------------- |
@@ -58,31 +56,31 @@ Within the project there is a file `/data/dbus-ha-evcharger/config.ini` - just c
 | DEFAULT  | Host | IP or hostname of the homeassistant api |
 | DEFAULT  | Token | Long lived token from homeassistant/profile/security |
 
-python /data/dbus-ha-evcharger/dbus-ha-evcharger.py
+python /data/dbus-ha-heatpump/dbus-ha-heatpump.py
 
 
 ### Debugging
 You can check the status of the service with svstat:
 
-svstat /service/dbus-ha-evcharger
+svstat /service/dbus-ha-heatpump
 
 ### Also useful:
 
-tail -f /data/dbus-ha-evcharger/current.log 
+tail -f /data/dbus-ha-heatpump/current.log 
 
 ### Stop the script
-svc -d /service/dbus-ha-evcharger
+svc -d /service/dbus-ha-heatpump
 
 ### Start the script
-svc -u /service/dbus-ha-evcharger
+svc -u /service/dbus-ha-heatpump
 
 ### Restart the script
 If you want to restart the script, for example after changing it, just run the following command:
 
-sh /data/dbus-ha-evcharger/restart.sh
+sh /data/dbus-ha-heatpump/restart.sh
 
 ## Used documentation
 - https://github.com/victronenergy/venus/wiki/dbus#grid   DBus paths for Victron namespace GRID
-- https://github.com/victronenergy/venus/wiki/dbus#evcharger  DBus paths for Victron namespace EVCHARGER
+- https://github.com/victronenergy/venus/wiki/dbus#heatpump  DBus paths for Victron namespace heatpump
 - https://github.com/victronenergy/venus/wiki/dbus-api   DBus API from Victron
 - https://www.victronenergy.com/live/ccgx:root_access   How to get root access on GX device/Venus OS
--- dbus-ha-heatpump.py
+++ dbus-ha-heatpump.py
@@ -26,7 +26,7 @@ class DbusHAHeatpumpService:
         deviceinstance = int(config['DEFAULT']['DeviceInstance'])
         customname = config['DEFAULT']['CustomName']
 
-        productid = 0xFFFF #45069
+        productid = 0xFFFF 
 
         self._dbusservice = VeDbusService("{}.http_{:02d}".format(servicename, deviceinstance))
         self._paths = paths
@@ -119,7 +119,6 @@ class DbusHAHeatpumpService:
     def _signOfLife(self):
         logging.info("--- Start: sign of life ---")
         logging.info("Last _update() call: %s" % (self._lastUpdate))
-        logging.info("Last '/Ac/Power': %s" % (self._dbusservice['/Ac/Power']))
         logging.info("--- End: sign of life ---")
         return True
  
@@ -131,67 +130,32 @@ class DbusHAHeatpumpService:
             hp_data = self._getData()
             config = self._getConfig()
 
-
-#/ProductId
-#/State. (0,1)
-#/Ac/Power
-#/Ac/Energy/Forward
-#/Temperature
-#/TargetTemperature
-
             #send data to DBus for 3phase system
-            self._dbusservice['/Ac/Power'] = ev_data['power']
-            self._dbusservice['/Current'] = ev_data['l1_i']
-            self._dbusservice['/Ac/L1/Power'] = ev_data['l1_v']*ev_data['l1_i']
-            self._dbusservice['/Ac/L2/Power'] = ev_data['l2_v']*ev_data['l2_i']
-            self._dbusservice['/Ac/L3/Power'] = ev_data['l3_v']*ev_data['l3_i']
-            self._dbusservice['/Mode'] = 0  # Manual, no control
-            self._dbusservice['/MaxCurrent'] = 16
-            #self._dbusservice['/Status'] = 1  # 0=Disconnected, 1=Connected, 2=Charging, 3=Charged, #24=Stop Charging
-
-
-            # set charging time start
-            if self._charging_time["start"] is None and ev_data['power'] > 0:
-                self._charging_time["start"] = now
-                self._energy_start = (ev_data['energy'])
-                self._dbusservice['/Status'] = 2 #Charging
-
-            # calculate charging time if charging started
-            if self._charging_time["start"] is not None:
-                self._dbusservice['/ChargingTime'] = now - self._charging_time["start"]
-
-                if ev_data['power'] == 0 and self._charging_time["stopped_since"] is None:
-                    self._charging_time["stopped_since"] = now
-                    self._dbusservice['/Status'] = 1 #Connected
-                elif ev_data['power'] > 0 and self._charging_time["stopped_since"] is not None:
-                    self._charging_time["stopped_since"] = None
-
-                if self._charging_time["stopped_since"] is not None and STOP_CHARGING_COUNTER_AFTER < now - self._charging_time["stopped_since"]:
-                    self._charging_time["start"] = None
-                    self._charging_time["stopped_since"] = None
-                    self._dbusservice['/ChargingTime']= None
-            
-            if self._energy_start > 0:
-                self._dbusservice['/Ac/Energy/Forward'] = (ev_data['energy']) - self._energy_start
+            self._dbusservice['/Ac/Power'] = hp_data['power']
+            self._dbusservice['/Ac/Energy/Forward'] = hp_data['energy']
+            self._dbusservice['/Temperature'] = hp_data['current_temperature']
+            self._dbusservice['/TargetTemperature'] = hp_data['target_temperature']
+            if( hp_data['state'] == 'on' )"
+                self._dbusservice['/State'] = 1
             else:
-                self._dbusservice['/Ac/Energy/Forward'] = 0
-                
-            #logging
-            logging.debug("Grid Consumption (/Ac/Power): %s" % (self._dbusservice['/Ac/Power']))
-            logging.debug("Grid Forward (/Ac/Energy/Forward): %s" % (self._dbusservice['/Ac/Energy/Forward']))
-            logging.debug("---");
-            
+                self._dbusservice['/State'] = 10
+
             # increment UpdateIndex - to show that new data is available an wrap
             self._dbusservice['/UpdateIndex'] = (self._dbusservice['/UpdateIndex'] + 1 ) % 256
-
             #update lastupdate vars
             self._lastUpdate = time.time()
+
+            #logging
+            logging.debug("Heatpump Power (/Ac/Power): %s" % (self._dbusservice['/Ac/Power']))
+            logging.debug("Heatpump Energy (/Ac/Energy/Forward): %s" % (self._dbusservice['/Ac/Energy/Forward']))
+            logging.debug("---");
         except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.Timeout, ConnectionError) as e:
             logging.critical('Error getting data from ESPHome - check network or ESPhome device status. Setting power values to 0. Details: %s', e, exc_info=e)       
-            self._dbusservice['/Ac/L1/Power'] = 0                                       
-            self._dbusservice['/Ac/L2/Power'] = 0                                       
-            self._dbusservice['/Ac/L3/Power'] = 0
             self._dbusservice['/Ac/Power'] = 0
+            self._dbusservice['/Ac/Energy/Forward'] = 0
+            self._dbusservice['/Temperature'] = 0
+            self._dbusservice['/TargetTemperature] = 0
+            self._dbusservice['/State'] = 0  # 0=off, 1=on
             self._dbusservice['/UpdateIndex'] = (self._dbusservice['/UpdateIndex'] + 1 ) % 256        
         except Exception as e:
             logging.critical('Error at %s', '_update', exc_info=e)
@@ -237,7 +201,8 @@ def main():
         _kwh = lambda p, v: (str(round(v, 2)) + ' kWh')
         _a = lambda p, v: (str(round(v, 1)) + ' A')
         _w = lambda p, v: (str(round(v, 1)) + ' W')
-        _v = lambda p, v: (str(round(v, 1)) + ' V')   
+        _v = lambda p, v: (str(round(v, 1)) + ' V')  
+        _t = lambda p, v: (str(round(v, 1)) + '°C')
         _n = lambda p, v: (str("%i" % v ) )
         
         #start our main-service
@@ -246,19 +211,10 @@ def main():
             servicename='com.victronenergy.evcharger.ha',
             paths={
                 '/Ac/Energy/Forward': {'initial': 0, 'textformat': _kwh}, # energy bought from the grid
-                '/Ac/Power': {'initial': 0, 'textformat': _w},            
-                '/Ac/Current': {'initial': 0, 'textformat': _a},
-                '/Ac/L1/Power': {'initial': 0, 'textformat': _w},
-                '/Ac/L2/Power': {'initial': 0, 'textformat': _w},
-                '/Ac/L3/Power': {'initial': 0, 'textformat': _w},
-                "/Current": {'initial': None, "textformat": _a},
-                "/MaxCurrent": {'initial': None, "textformat": _a},
-                "/SetCurrent": {'initial': None, "textformat": _a},
-                "/AutoStart": {'initial': 0, "textformat": _n},
-                "/ChargingTime": {'initial': None, "textformat": _n},                
-                '/Mode': {'initial': 1, "textformat": _n},
-                "/Status": {'initial': 0, "textformat": _n},
-                "/StartStop": {'initial': 1, "textformat": _n},
+                '/Ac/Power': {'initial': 0, 'textformat': _w}, 
+                '/State': {'initial': 0, "textformat": _n},           
+                "/Temperature": {'initial': 0, "textformat": _t},
+                "/TargetTemperature": {'initial': 0, "textformat": _t},
                 })
         logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
         mainloop = gobject.MainLoop()
